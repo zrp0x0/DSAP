139e16a5c30194e17635e4d7f5a3c19bf5e74898
zrp <zrp0x0@gmail.com>
2026-02-24 16:07:39 +0900

    [Add] 0x09 응용문제 1 2번 완료

--- x09/a1.cpp
+++ x09/a1.cpp
@@ -2,13 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
-
-int N, M;
-
 char board[1002][1002];
-int dist[1002][1002][2];
+int vis[1002][1002][2];
 
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
@@ -17,44 +12,52 @@ int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N, M;
     cin >> N >> M;
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cin >> board[i][j];
-            board[i][j] = board[i][j] - '0';
-            dist[i][j][0] = dist[i][j][1] = -1;
         }
     }
 
     queue<tuple<int, int, int>> Q;
-    dist[0][0][0] = 1;
-    dist[0][0][1] = 1;
     Q.push({0, 0, 0});
+    vis[0][0][0] = 1;
 
     while (!Q.empty()) {
         tuple<int, int, int> cur = Q.front();
         Q.pop();
 
         if (get<0>(cur) == N - 1 && get<1>(cur) == M - 1) {
-            cout << dist[get<0>(cur)][get<1>(cur)][get<2>(cur)] << '\n';
+            cout << vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] << '\n';
+            // for (int x = 0; x < N; x++) {
+            //     for (int y = 0; y < M; y++) {
+            //         cout << vis[x][y][get<2>(cur)] << ' ';
+            //     }
+            //     cout << '\n';
+            // }
             return 0;
         }
 
         for (int k = 0; k < 4; k++) {
             int nx = get<0>(cur) + dx[k];
             int ny = get<1>(cur) + dy[k];
-            int brk = get<2>(cur);
+            int broken = get<2>(cur);
 
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-            if (board[nx][ny] == 0 && dist[nx][ny][brk] == -1) {
-                dist[nx][ny][brk] = dist[get<0>(cur)][get<1>(cur)][brk] + 1;
-                Q.push({nx, ny, brk});
+            
+            if (board[nx][ny] == '1' && broken == 0 && vis[nx][ny][1] == 0) { // 벽에 막혀있고, 아직 한 번도 부수지 않았다면
+                Q.push({nx, ny, 1});
+                vis[nx][ny][1] = vis[get<0>(cur)][get<1>(cur)][broken] + 1;
+                continue;
+            } else if (board[nx][ny] == '1' && broken == 1) {
+                continue;
             }
 
-            if (!brk && board[nx][ny] == 1 && dist[nx][ny][1] == -1) {
-                dist[nx][ny][1] = dist[get<0>(cur)][get<1>(cur)][brk] + 1;
-                Q.push({nx, ny, 1});
+            if (board[nx][ny] == '0' && vis[nx][ny][broken] == 0) { 
+                Q.push({nx, ny, broken});
+                vis[nx][ny][broken] = vis[get<0>(cur)][get<1>(cur)][broken] + 1;
             }
         }
     }
--- x09/a2.cpp
+++ x09/a2.cpp
@@ -3,7 +3,53 @@
 using namespace std;
 
 int arr[100002];
-int vis[100002];
+int sta[100002];
+
+const int NOT_VISITED = 0;
+const int VISITED = 1;
+const int CYCLE_IN = 2;
+const int NOT_CYCLE_IN = 3;
+
+void run(int x) {
+    int cur = x;
+    while (true) {
+        sta[cur] = VISITED;
+        cur = arr[cur];
+
+        if (sta[cur] == CYCLE_IN || sta[cur] == NOT_CYCLE_IN) {
+            cur = x;
+            while (sta[cur] == VISITED) {
+                sta[cur] = NOT_CYCLE_IN;
+                cur = arr[cur];
+            }
+
+            return;
+        }
+
+        if (sta[cur] == VISITED && cur != x) {
+            while (sta[cur] != CYCLE_IN) {
+                sta[cur] = CYCLE_IN;
+                cur = arr[cur];
+            }
+
+            cur = x;
+            while (sta[cur] != CYCLE_IN) {
+                sta[cur] = NOT_CYCLE_IN;
+                cur = arr[cur];
+            }
+
+            return ;
+        }
+
+        if (sta[cur] == VISITED && cur == x) {
+            while (sta[cur] != CYCLE_IN) {
+                sta[cur] = CYCLE_IN;
+                cur = arr[cur];
+            }
+            return;
+        }
+    }
+}
 
 int main(void) {
     ios_base::sync_with_stdio(false);
@@ -15,17 +61,23 @@ int main(void) {
         int N;
         cin >> N;
         for (int i = 1; i <= N; i++) {
-            cin >> arr[i];
-            vis[i] = 0;
+            sta[i] = 0;
         }
-
-        queue<int> Q;
 
         for (int i = 1; i <= N; i++) {
-            if (vis[i] == 0) {
-
-            }
+            cin >> arr[i];
         }
+
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            if (sta[i] == NOT_VISITED) run(i);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (sta[i] == NOT_CYCLE_IN) ans++;
+        }
+
+        cout << ans << '\n';
     }
 
     return 0;
